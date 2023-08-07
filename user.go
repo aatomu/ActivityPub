919b2b70b75eeb9335d412450a610ab97c6bb04e
atomu21263 <aatomu21263+git@gmail.com>
2023-08-08 01:04:54 +0900

    デフォルト追加

--- user.go
+++ user.go
@@ -136,6 +136,8 @@ func inboxEventUndo(w http.ResponseWriter, userID string, undoActivity ActivityS
 
 		w.WriteHeader(http.StatusAccepted)
 		return
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
 	}
 }
 
